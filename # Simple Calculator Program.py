a9bd1eafdf55159a6bf9a6c929e70cd5bfb98d1d
MwangiMary <mary99mwangi@gmail.com>
2025-07-30 17:17:45 +0300

    Initial commit

---
+++ # Simple Calculator Program.py
@@ -0,0 +1,19 @@
+# Simple Calculator Program
+
+# Get inputs from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform calculation
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    result = num1 / num2
+
+# Display result
+print(f"{num1} {operation} {num2} = {result}")
